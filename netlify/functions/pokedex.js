3919067c7c5f61cea45ba449c9c0345cf3adf35e
Hunter Trammell <hunterttrammell@gmail.com>
2021-07-27 22:58:09 -0500

    feature: add pokedex api

---
+++ netlify/functions/pokedex.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch')
+
+exports.handler = async function(){
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+
+    const response = await fetch(POKE_API)
+    const data = await response.json()
+    const list = data.pokemon_entries.map(pokemon => {return {
+        id: pokemon.entry_number, 
+        name: pokemon.pokemon_species.name
+    }})
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            data: list
+        })
+    }
+}
